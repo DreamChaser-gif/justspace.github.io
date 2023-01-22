a3930b64c9c57db8e335280993bd42c716eaa7c2
DreamChaser-gif <123293885+DreamChaser-gif@users.noreply.github.com>
2023-01-22 16:12:41 +0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,27 @@
+// // Get a reference to the buttons
+// const button1 = document.getElementById("falcon9");
+// const button2 = document.getElementById("falconheavy");
+// const button3 = document.getElementById("starship");
+// const button4 = document.getElementById("starlink");
+
+// // Add event listeners to the buttons
+// button1.addEventListener("click", function () {
+//     document.body.style.backgroundImage = "url(falcon_9.jpg)";
+//     displayText.innerHTML = "You clicked falcon 9"
+// }); 
+
+// button2.addEventListener("click", function () {
+//     document.body.style.backgroundImage = "url(fh.jpg)";
+// });
+
+// button3.addEventListener("click", function () {
+//     document.body.style.backgroundImage = "url(star.jpp)";
+// });
+
+// button4.addEventListener("click", function () {
+//     document.body.style.backgroundImage = "url(starlink.jpg)";
+// });
+
+
+
+
